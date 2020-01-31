443bdd058f7979f0e9edc68e76153eb483e8bc3f
bzxnaga <bzxnaga@gmail.com>
2020-01-31 11:39:30 +0700

    11:39

---
+++ 4.Mengenal Pengaturan Conditional Switch-Case.js
@@ -0,0 +1,25 @@
+ΣF = resultan gaya (Newton)
+m = massa benda (kg)
+a = percepatan benda (m/s2)
+
+Ketika 3 orang maka  600kg x 2m/s2
+
+
+
+// STORE "ΣF" without any value
+// STORE "Massa" without any value
+// STORE "Massa Percepatan 3 orang" with 2
+// STORE "Percepatan" with 600
+//
+
+// CALCULATE "Massa" times "Percepatan 3 orang"
+// SET "ΣF" value with calculation result
+
+// DISPLAY "ΣF"
+
+
+
+
+
+
+
